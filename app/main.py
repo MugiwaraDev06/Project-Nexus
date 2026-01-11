df586d16f899efcbe7ed96f09a0b0ff5cb41418e
Mugiwara <siddarthbhardwaj612@gmail.com>
2026-01-11 21:29:17 +0530

    setup auth API structure and login placeholder

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+from app.api.auth.routes import router as auth_router
 from fastapi import FastAPI
 
 app = FastAPI(
     title="Project Nexus",
     version="0.1.0",
 )
+app.include_router(auth_router)
 
 
 @app.get("/health")
